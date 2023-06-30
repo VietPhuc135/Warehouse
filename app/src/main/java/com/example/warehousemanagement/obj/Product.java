219e2a1fd502e51b960d7f95483ab8148c9bc46f
DoThanhh83 <100203762+DoThanhh83@users.noreply.github.com>
2023-06-30 16:16:06 +0700

    ok

--- app/src/main/java/com/example/warehousemanagement/obj/Product.java
+++ app/src/main/java/com/example/warehousemanagement/obj/Product.java
@@ -1,7 +1,15 @@
 package com.example.warehousemanagement.obj;
 
 public class Product {
+    public String getId() {
+        return id;
+    }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+          private  String id ;
         private String name;
         private String code;
         private String date;
