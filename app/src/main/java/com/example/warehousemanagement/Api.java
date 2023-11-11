8140c746abc9e869c9060f8f5c3cf4e072bb54c9
VietPhuc135 <ttvietphuc135@gmail.com>
2023-11-11 19:15:53 +0700

    test

--- app/src/main/java/com/example/warehousemanagement/Api.java
+++ app/src/main/java/com/example/warehousemanagement/Api.java
@@ -1,5 +1,5 @@
 package com.example.warehousemanagement;
 
 public class Api {
-    public static String baseURL = "http://192.168.1.5:8080";
+    public static String baseURL = "http://192.168.1.6:8080";
 }
